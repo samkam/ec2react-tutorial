a1eb94d085313908b83f531990c76d599da4af44
Sam Kamenetz <samkamenetz@gmail.com>
2019-04-26 21:15:19 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hi I made this. I am Sam.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
